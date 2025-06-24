6fb5d715940bf5330e56098d4a20ebb32fbbce38
margaritanaumova <naumovamv@gmail.com>
2025-06-24 20:45:10 +0200

    Создание TestBase и GroupData.

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,62 +1,33 @@
-import org.junit.jupiter.api.BeforeEach;
+import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTests {
-  private static WebDriver driver;
-
-  @BeforeEach
-  public void setUp() {
-    if (driver == null) {
-      driver = new FirefoxDriver();
-      Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-      driver.get("http://localhost/addressbook/");
-      driver.findElement(By.name("user")).click();
-      driver.findElement(By.name("user")).sendKeys("admin");
-      driver.findElement(By.name("pass")).sendKeys("secret");
-      driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-    }
-  }
-
-  private boolean isElementPresent(By locator) {
-    try {
-      driver.findElement(locator);
-      return true;
-    } catch (NoSuchElementException exception) {
-      return false;
-    }
-  }
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void canCreateGroupTest() {
-    if (!isElementPresent(By.name("new"))) {
-      driver.findElement(By.linkText("groups")).click();
-    }
+    openGroupPage();
+    createGroup(new GroupData("test", "test", "test"));
+  }
 
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("Group name");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("Group header");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("Group footer");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("groups")).click();
+  @Test
+  public void canCreateGroupWithEmptyNameTest() {
+    openGroupPage();
+    var emptyGroup = new GroupData();
+    var groupWithName = emptyGroup.withName("some name");
+    createGroup(groupWithName);
+  }
+
+  @Test
+  public void canCreateGroupWithNameOnlyTest() {
+    openGroupPage();
+    createGroup(new GroupData().withName("some name"));
   }
 
   @Test
   public void canDeleteGroupTest() {
-    if (!isElementPresent(By.name("new"))) {
-      driver.findElement(By.linkText("groups")).click();
-    }
-    if (!isElementPresent(By.name("selected[]"))) {
-      canCreateGroupTest();
-    }
-    driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.name("delete")).click();
-    driver.findElement(By.linkText("group page")).click();
+    openGroupPage();
+    isGroupPresent();
+    removeGroup();
   }
+
 }
---
+++ addressbook_web_tests/src/test/java/model/GroupData.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.Objects;
+
+public record GroupData(String name, String header, String footer) {
+
+  public GroupData() {
+    this("", "", "");
+  }
+
+  public GroupData withName(String name) {
+    return new GroupData(name, this.header, this.footer);
+  }
+
+  public GroupData withHeader(String header) {
+    return new GroupData(this.name, header, this.footer);
+  }
+
+  public GroupData withFooter(String footer) {
+    return new GroupData(this.name, this.header, footer);
+  }
+}
